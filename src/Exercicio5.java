0ca290907f76c652a116a038d4fb3baaa333cd4d
willbertini <bertini@gmail.com>
2024-04-14 22:18:30 -0300

    Create Exercicio5.java

---
+++ src/Exercicio5.java
@@ -0,0 +1,17 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+        String nomeProduto1 = "Livro";
+        String nomeProduto2 = "Celular";
+
+        double precoProduto1 = 49.95;
+        double precoProduto2 = 1470.76;
+
+        System.out.println(">>> Listagem dos Produtos");
+        System.out.println("____________________________________");
+        System.out.println("Nome:" + nomeProduto1);
+        System.out.println("Preço: R$" + precoProduto1);
+        System.out.println("____________________________________");
+        System.out.println("Nome:" + nomeProduto2);
+        System.out.println("Preço: R$" + precoProduto2);
+    }
+}
