4e3eb2fe6151b8d2a6882d4f5b732a957849f3a2
willbertini <bertini@gmail.com>
2024-04-14 21:58:55 -0300

    Create Exercicio4.java

---
+++ src/Exercicio4.java
@@ -0,0 +1,16 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        String nomeCliente1 = "Fulano";
+        String nomeCliente2 = "Ciclano";
+        int dataNascimentoCliente1 = 1980;
+        int dataNascimentoCliente2 = 2000;
+
+        System.out.println(">>> Listagem dos Clientes");
+        System.out.println("____________________________________");
+        System.out.println("Nome:" + nomeCliente1);
+        System.out.println("Data de Nascimento: " + dataNascimentoCliente1);
+        System.out.println("____________________________________");
+        System.out.println("Nome:" + nomeCliente2);
+        System.out.println("Data de Nascimento: " + dataNascimentoCliente2);
+    }
+}
